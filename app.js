038e3446a7d01a99015ad7ad6d4204091488054c
Dabkdn <nkh1111202@gmail.com>
2019-11-07 10:19:19 +0700

    static folder public for storage files

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors());
 app.use('/api', apiRouter)
+app.use(express.static('public'))
 app.get('*', function(req, res){
     res.status(404).render('notfound');
 });
